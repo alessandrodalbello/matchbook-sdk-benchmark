3fb8e19d04475a886c1a312cd22a348444c0cbfc
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2019-09-15 20:44:52 +0200

    Fix build installing provided JAR's to local Maven

---
+++ deserialiser-obj-mapper/src/main/java/org/alessandrodalbello/GetEventsBenchmark.java
@@ -0,0 +1,7 @@
+package org.alessandrodalbello;
+
+/**
+ * Benchmark of Matchbook SDK version that uses a Jackson ObjectMapper deserialisation implementation.
+ */
+public class GetEventsBenchmark {
+}
