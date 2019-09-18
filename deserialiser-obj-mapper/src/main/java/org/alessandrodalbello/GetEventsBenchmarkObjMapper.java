3bd1cb357ed2c88660756309bc45ae25f653bee7
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2019-09-18 22:49:02 +0200

    Configure Wiremock to mock server responses

--- deserialiser-obj-mapper/src/main/java/org/alessandrodalbello/GetEventsBenchmarkObjMapper.java
+++ deserialiser-obj-mapper/src/main/java/org/alessandrodalbello/GetEventsBenchmarkObjMapper.java
@@ -1,12 +1,16 @@
 package org.alessandrodalbello;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.matchbook.sdk.core.StreamObserver;
 import com.matchbook.sdk.core.exceptions.MatchbookSDKException;
 import com.matchbook.sdk.rest.ClientConfig;
@@ -15,6 +19,7 @@ import com.matchbook.sdk.rest.EventsClient;
 import com.matchbook.sdk.rest.EventsClientRest;
 import com.matchbook.sdk.rest.dtos.events.Event;
 import com.matchbook.sdk.rest.dtos.events.EventsRequest;
+import org.eclipse.jetty.http.HttpStatus;
 import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -26,6 +31,7 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
@@ -40,11 +46,16 @@ public class GetEventsBenchmarkObjMapper {
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void getOneTennisEvent(ExecutionPlan executionPlan, Blackhole blackhole) {
+    public void getFiftyEventsWithParticipants(ExecutionPlan executionPlan, Blackhole blackhole) {
+        executionPlan.wireMockServer.stubFor(get(urlPathEqualTo("/edge/rest/events"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK_200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("50_events_with_participants_and_prices.json")));
+
         EventsRequest eventsRequest = new EventsRequest.Builder()
-                .perPage(1)
-                .sportIds(Collections.singleton(9L))
-                .includeEventParticipants(false)
+                .includeEventParticipants(true)
+                .perPage(50)
                 .build();
         WaitingStreamObserver<Event> waitingStreamObserver = new WaitingStreamObserver<>(blackhole);
         executionPlan.eventsClient.getEvents(eventsRequest, waitingStreamObserver);
@@ -55,13 +66,30 @@ public class GetEventsBenchmarkObjMapper {
     @State(Scope.Benchmark)
     public static class ExecutionPlan {
 
+        private WireMockServer wireMockServer;
         private EventsClient eventsClient;
 
         @Setup(Level.Trial)
         public void setUp() {
-            ClientConfig clientConfig = new ClientConfig.Builder("anonymous".toCharArray(), null).build();
-            ConnectionManager connectionManager = new ConnectionManager.Builder(clientConfig).build();
+            WireMockConfiguration wireMockServerConfiguration = WireMockConfiguration.wireMockConfig()
+                    .disableRequestJournal()
+                    .port(8080);
+            wireMockServer = new WireMockServer(wireMockServerConfiguration);
+            wireMockServer.start();
+
+            ClientConfig clientConfig = new ClientConfig.Builder("anonymous".toCharArray(), "secret".toCharArray())
+                    .sportsUrl("http://localhost:8080/edge/rest")
+                    .loginUrl("http://localhost:8080/bpapi/rest/security/session")
+                    .build();
+            ConnectionManager connectionManager = new ConnectionManager.Builder(clientConfig)
+                    .sessionAutoManage(false)
+                    .build();
             eventsClient = new EventsClientRest(connectionManager);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() {
+            wireMockServer.stop();
         }
     }
 
