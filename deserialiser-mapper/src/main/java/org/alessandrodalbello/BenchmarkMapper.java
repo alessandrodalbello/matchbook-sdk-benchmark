8e5fbec5f45f23e83232089a3cd1c1cca172fa0b
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2019-09-22 20:48:52 +0200

    Parameterise benchmarks to minimise test code

--- deserialiser-mapper/src/main/java/org/alessandrodalbello/BenchmarkMapper.java
+++ deserialiser-mapper/src/main/java/org/alessandrodalbello/BenchmarkMapper.java
@@ -1,20 +1,8 @@
 package org.alessandrodalbello;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-import java.util.concurrent.CountDownLatch;
-
-import com.matchbook.sdk.core.StreamObserver;
-import com.matchbook.sdk.core.exceptions.MatchbookSDKException;
-import com.matchbook.sdk.rest.dtos.events.Event;
-import com.matchbook.sdk.rest.dtos.events.EventRequest;
-import com.matchbook.sdk.rest.dtos.events.EventsRequest;
-import org.eclipse.jetty.http.HttpStatus;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -25,130 +13,17 @@ import org.openjdk.jmh.annotations.Timeout;
 import org.openjdk.jmh.annotations.Warmup;
 
 @Fork(value = 1, warmups = 0)
-@Warmup(iterations = 3, time = 5, timeUnit = SECONDS)
-@Measurement(iterations = 10, time = 5, timeUnit = SECONDS)
+@Warmup(iterations = 5, time = 5, timeUnit = SECONDS)
+@Measurement(iterations = 20, time = 5, timeUnit = SECONDS)
 @Timeout(time = 30, timeUnit = SECONDS)
 @OutputTimeUnit(MILLISECONDS)
 public class BenchmarkMapper {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public void get20EventsWithoutPrices_OverallTime(BenchmarkStateMapper benchmarkState) {
-        getEvents(benchmarkState, "20_events_without_prices.json", 2, true);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void get20EventsWithoutPrices_TimeToFirst(BenchmarkStateMapper benchmarkState) {
-        getEvents(benchmarkState, "20_events_without_prices.json", 2, false);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void get20EventsWithPrices_OverallTime(BenchmarkStateMapper benchmarkState) {
-        getEvents(benchmarkState, "20_events_with_prices.json", 2, true);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void get20EventsWithPrices_TimeToFirst(BenchmarkStateMapper benchmarkState) {
-        getEvents(benchmarkState, "20_events_with_prices.json", 2, false);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void get50EventsWithParticipantsAndPrices_OverallTime(BenchmarkStateMapper benchmarkState) {
-        getEvents(benchmarkState, "50_events_with_participants_and_prices.json", 3, true);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void get50EventsWithParticipantsAndPrices_TimeToFirst(BenchmarkStateMapper benchmarkState) {
-        getEvents(benchmarkState, "50_events_with_participants_and_prices.json", 3, false);
-    }
-
-    private void getEvents(BenchmarkStateMapper benchmarkState, String responseFile, int waitTimeout, boolean waitCompletion) {
-        benchmarkState.wireMockServer.stubFor(get(urlPathEqualTo("/edge/rest/events"))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK_200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBodyFile(responseFile)));
-
-        EventsRequest eventsRequest = new EventsRequest.Builder().build();
-        WaitStreamObserver streamObserver = new WaitStreamObserver(waitCompletion);
-        benchmarkState.eventsClient.getEvents(eventsRequest, streamObserver);
-
-        streamObserver.await(waitTimeout);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void getSingleEvent_OverallTime(BenchmarkStateMapper benchmarkState) {
-        getEvent(benchmarkState, true);
-    }
-
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    public void getSingleEvent_TimeToFirst(BenchmarkStateMapper benchmarkState) {
-        getEvent(benchmarkState, false);
-    }
-
-    private void getEvent(BenchmarkStateMapper benchmarkState, boolean waitCompletion) {
-        benchmarkState.wireMockServer.stubFor(get(urlPathMatching("/edge/rest/events/\\d+"))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.OK_200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBodyFile("single_event.json")));
-
-        EventRequest eventRequest = new EventRequest.Builder(1227568266270017L).build();
-        WaitStreamObserver streamObserver = new WaitStreamObserver(waitCompletion);
-        benchmarkState.eventsClient.getEvent(eventRequest, streamObserver);
-
-        streamObserver.await(1);
-    }
-
-    private static class WaitStreamObserver implements StreamObserver<Event> {
-
-        private final boolean waitCompletion;
-        private final CountDownLatch countDownLatch;
-
-        private Exception matchbookSDKException;
-
-        private WaitStreamObserver(boolean waitCompletion) {
-            this.waitCompletion = waitCompletion;
-            countDownLatch = new CountDownLatch(1);
-        }
-
-        private void await(int waitTimeout) {
-            try {
-                countDownLatch.await(waitTimeout, SECONDS);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            if (matchbookSDKException != null) {
-                throw new RuntimeException(matchbookSDKException);
-            }
-        }
-
-        @Override
-        public void onNext(Event event) {
-            if (!waitCompletion) {
-                countDownLatch.countDown();
-            }
-        }
-
-        @Override
-        public void onCompleted() {
-            if (waitCompletion) {
-                countDownLatch.countDown();
-            }
-        }
-
-        @Override
-        public <E extends MatchbookSDKException> void onError(E matchbookSDKException) {
-            this.matchbookSDKException = matchbookSDKException;
-            countDownLatch.countDown();
-        }
+    public void getEvents(BenchmarkStateMapper benchmarkState) {
+        benchmarkState.eventsClient.getEvents(benchmarkState.eventsRequest, benchmarkState.streamObserver);
+        benchmarkState.streamObserver.await();
     }
 
 }
--- deserialiser-mapper/src/main/java/org/alessandrodalbello/BenchmarkStateMapper.java
+++ deserialiser-mapper/src/main/java/org/alessandrodalbello/BenchmarkStateMapper.java
@@ -1,12 +1,25 @@
 package org.alessandrodalbello;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.concurrent.CountDownLatch;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.matchbook.sdk.core.StreamObserver;
+import com.matchbook.sdk.core.exceptions.MatchbookSDKException;
 import com.matchbook.sdk.rest.ClientConfig;
 import com.matchbook.sdk.rest.ConnectionManager;
 import com.matchbook.sdk.rest.EventsClient;
 import com.matchbook.sdk.rest.EventsClientRest;
+import com.matchbook.sdk.rest.dtos.events.Event;
+import com.matchbook.sdk.rest.dtos.events.EventsRequest;
+import org.eclipse.jetty.http.HttpStatus;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -15,17 +28,37 @@ import org.openjdk.jmh.annotations.TearDown;
 @State(Scope.Benchmark)
 public class BenchmarkStateMapper {
 
-    WireMockServer wireMockServer;
     EventsClient eventsClient;
+    EventsRequest eventsRequest;
+    WaitStreamObserver streamObserver;
+
+    private WireMockServer wireMockServer;
+
+    @Param({
+            "1_event.json",
+            "20_events_without_prices.json",
+            "20_events_with_prices.json",
+            "50_events_with_participants_and_prices.json"
+    })
+    private String response;
+
+    @Param({"true", "false"})
+    private boolean waitCompletion;
 
     @Setup(Level.Trial)
-    public void setUp() {
+    public void setUpBenchmark() {
         WireMockConfiguration wireMockServerConfiguration = WireMockConfiguration.wireMockConfig()
                 .usingFilesUnderDirectory("src/main/resources")
                 .disableRequestJournal()
                 .port(8080);
         wireMockServer = new WireMockServer(wireMockServerConfiguration);
         wireMockServer.start();
+
+        wireMockServer.stubFor(get(urlPathEqualTo("/edge/rest/events"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK_200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile(response)));
 
         ClientConfig clientConfig = new ClientConfig.Builder("anonymous".toCharArray(), "secret".toCharArray())
                 .sportsUrl("http://localhost:8080/edge/rest")
@@ -35,11 +68,66 @@ public class BenchmarkStateMapper {
                 .sessionAutoManage(false)
                 .build();
         eventsClient = new EventsClientRest(connectionManager);
+        eventsRequest = new EventsRequest.Builder().build();
     }
 
     @TearDown(Level.Trial)
-    public void tearDown() {
+    public void tearDownBenchmark() {
         wireMockServer.shutdown();
+    }
+
+    @Setup(Level.Invocation)
+    public void setUpInvocation() {
+        streamObserver = new WaitStreamObserver(waitCompletion);
+    }
+
+    @TearDown(Level.Invocation)
+    public void tearDownInvocation() {
+        streamObserver = null;
+    }
+
+    static class WaitStreamObserver implements StreamObserver<Event> {
+
+        private final boolean waitCompletion;
+        private final CountDownLatch countDownLatch;
+
+        private Exception matchbookSDKException;
+
+        private WaitStreamObserver(boolean waitCompletion) {
+            this.waitCompletion = waitCompletion;
+            countDownLatch = new CountDownLatch(1);
+        }
+
+        void await() {
+            try {
+                countDownLatch.await(2, SECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (matchbookSDKException != null) {
+                throw new RuntimeException(matchbookSDKException);
+            }
+        }
+
+        @Override
+        public void onNext(Event event) {
+            if (!waitCompletion) {
+                countDownLatch.countDown();
+            }
+        }
+
+        @Override
+        public void onCompleted() {
+            if (waitCompletion) {
+                countDownLatch.countDown();
+            }
+        }
+
+        @Override
+        public <E extends MatchbookSDKException> void onError(E matchbookSDKException) {
+            this.matchbookSDKException = matchbookSDKException;
+            countDownLatch.countDown();
+        }
     }
 
 }
